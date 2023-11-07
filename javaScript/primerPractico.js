7163a57ecae533de4d762c4b011a45c5e7b286ab
Ezequiel Massa <ezequielmassa.dev@gmail.com>
2023-11-07 17:50:31 -0300

    creado primerPractio.js

--- javaScript/primerPractico.js
+++ javaScript/primerPractico.js
@@ -1,6 +1,6 @@
 // crear variables y mostrar en pantalla ya sea con un document.write o un alert o un console.log
 const nombre = 'Ezequiel'
 const apellido = 'Massa'
-let edad = 39
+let edad = 38
 const estatura = 1.75
 console.log(`Hola mi nombre es ${nombre} ${apellido} , tengo ${edad} años y mi estatura es ${estatura}`)
