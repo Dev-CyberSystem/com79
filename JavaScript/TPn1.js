8360273eddc41c16cfe8b55a1bd0871fa3813abf
FedericoFrau <fedeafrau@gmail.com>
2023-11-07 00:27:24 -0300

    últimas correcciones

--- JavaScript/TPn1.js
+++ JavaScript/TPn1.js
@@ -1,14 +1,24 @@
-let variableNueva1 = "valor A";
-let variableNueva2 = "1234";
-let valor1 = "Pepita";
-let valor2 = "la";
-let valor3 = "pistolera";
+function cartelAlerta(){
+    let variableNueva1 = "valor A";
+    let variableNueva2 = "1234";
 
-console.log("Texto de relleno con propósito de pruebas");
+    alert(variableNueva1 + " equivale a "+ variableNueva2);
+}
+cartelAlerta() //Hasta aquí mensaje de alerta
 
-console.log(valor1+" " + valor2 +" "+ valor3);
+function infoConsola(){
+    let valor1 = "Pepita";
+    let valor2 = "la";
+    let valor3 = "pistolera";
+    
+    console.log("Texto de relleno con propósito de pruebas");
+    
+    console.log(valor1+" " + valor2 +" "+ valor3);
+}
+infoConsola() //Hasta aquí info consola
 
-document.write("<br>"+"<hr>"+"Este es un texto agregado desde JS hoy lunes 06/11/2024"+"<hr>");
-document.write("<br>"+"<hr>"+"realizar F12 para revisar mensajes en consola por favor! ----->"+"<hr>");
-
-alert(variableNueva1 + " equivale a "+ variableNueva2);
+function textoAgregado() {
+    document.write("<br>"+"<hr>"+"Este es un texto agregado desde JS hoy lunes 06/11/2024"+"<hr>");
+    document.write("<br>"+"<hr>"+"realizar F12 para revisar mensajes en consola por favor! ----->"+"<hr>");
+}
+textoAgregado() //Hasta aquí texto agregado al .html
