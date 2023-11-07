ac0c315dd4965482242d47b828725365b009f98d
FedericoFrau <fedeafrau@gmail.com>
2023-11-07 01:00:54 -0300

    agregado adicional

--- JavaScript/TPn1.js
+++ JavaScript/TPn1.js
@@ -10,10 +10,10 @@ function infoConsola(){
     let valor1 = "Pepita";
     let valor2 = "la";
     let valor3 = "pistolera";
-    
+
     console.log("Texto de relleno con propósito de pruebas");
     
-    console.log(valor1+" " + valor2 +" "+ valor3);
+    console.log(valor1+" "+ valor2 +" "+ valor3);
 }
 infoConsola() //Hasta aquí info consola
 
@@ -22,3 +22,12 @@ function textoAgregado() {
     document.write("<br>"+"<hr>"+"realizar F12 para revisar mensajes en consola por favor! ----->"+"<hr>");
 }
 textoAgregado() //Hasta aquí texto agregado al .html
+
+function matemática(){
+    let text1 = "π";
+    const valorPi = "3,141592";
+    let propiedadA = "constante irracional";
+
+    document.write("<br>"+"La constante "+text1+" "+"posee un valor de "+valorPi+" "+"y por ser una "+propiedadA+" "+"sabemos que tendrá infinitos decimales.")
+}
+matemática() //Hasta aquí segundo texto agregado la .html
