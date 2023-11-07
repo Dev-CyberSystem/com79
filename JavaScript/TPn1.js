5cedce13437c6b8baa92f53651a890b0c9db4b5e
FedericoFrau <fedeafrau@gmail.com>
2023-11-07 00:06:36 -0300

    index y JavaScript

--- JavaScript/TPn1.js
+++ JavaScript/TPn1.js
@@ -1,2 +1,14 @@
-let variableNueva1 = "valor1";
+let variableNueva1 = "valor A";
 let variableNueva2 = "1234";
+let valor1 = "Pepita";
+let valor2 = "la";
+let valor3 = "pistolera";
+
+console.log("Texto de relleno con propósito de pruebas");
+
+console.log(valor1+" " + valor2 +" "+ valor3);
+
+document.write("<br>"+"<hr>"+"Este es un texto agregado desde JS hoy lunes 06/11/2024"+"<hr>");
+document.write("<br>"+"<hr>"+"realizar F12 para revisar mensajes en consola por favor! ----->"+"<hr>");
+
+alert(variableNueva1 + " equivale a "+ variableNueva2);
