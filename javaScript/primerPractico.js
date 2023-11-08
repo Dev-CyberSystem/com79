dba53f9163cdb5e8d27a355575653c3a72e5eb56
AvalosJorge <xjorgeluis93x@gmail.com>
2023-11-07 23:39:23 -0300

    Primer commit Js

---
+++ javaScript/primerPractico.js
@@ -0,0 +1,12 @@
+let nombre = "Avalos Jorge";
+
+console.log(nombre);
+
+document.write("Hola mi nombre es: " + nombre);
+
+alert("Aqui Jorge desde Js");
+
+function funcionAlertNombre() {
+
+    alert("Aqui Jorge desde Js");
+  }
