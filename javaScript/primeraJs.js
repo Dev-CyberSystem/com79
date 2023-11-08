d6ab279783ccc63bc27a14600cb21749fe11ed4c
willytucuman <tomasgroso09@hotmail.com>
2023-11-08 00:39:53 -0300

    commiteada 1 de js

--- javaScript/primeraJs.js
+++ javaScript/primeraJs.js
@@ -16,3 +16,14 @@ funcionAnonima();
 // console.log(nombre);
 
 // crear variables y mostrar en pantalla ya sea con un document.write o un alert o un console.log
+let edad = prompt("ingrese su edad")
+let profesion = "estudiante";
+console.log("yo soy "+{profesion} +"de programacion")
+let array = ["pancho","rocco","willy","milo"]
+for (i=0;i<array.length;i++){
+  console.log(array[i])
+}
+// if(edad >= 18){
+//   alert("mayor puede pasar porque tiene" +{edad}+ "años")
+// }  alert("menor puede pasar porque tiene" +{edad}+ "años")
+{edad >=18 ? alert("mayor puede pasar porque tiene" +{edad}+ "años") : alert("menor puede pasar porque tiene" +{edad}+ "años")}
