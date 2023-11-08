6ef92e63b888d6b955118d9f74eaddae90360af9
Ariana Alderete <alderete.ariana.1406@gmail.com>
2023-11-07 23:48:37 -0300

    primer commit de js

--- javaScript/primerPractico.js
+++ javaScript/primerPractico.js
@@ -6,13 +6,16 @@ document.write(leyenda);
 
 
 let nombre1 = "Queenie"
-let nombre2 = "Daryl"
-let nombre3 = "Michonne"
+let nombre2 = "Minini"
+let nombre3 = "Daryl"
+let nombre4 = "Michonne"
 
 function mostrarNombreDeMichis(){
     console.log("El nombre del primer michi es: "+nombre1)
     console.log("El nombre del segundo michi es: "+nombre2)
-    console.log("El nombre del tercero michi es: "+nombre3)
+    console.log("El nombre del tercer michi es: "+nombre3)
+    console.log("El nombre del cuarto michi es: "+nombre4)
+
 }
 mostrarNombreDeMichis();
 
