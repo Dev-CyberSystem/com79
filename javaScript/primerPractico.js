090f08dd0727f7b881b0a197e3eae7e9760b58ac
Ariana Alderete <alderete.ariana.1406@gmail.com>
2023-11-07 23:38:32 -0300

    primer commit js

---
+++ javaScript/primerPractico.js
@@ -0,0 +1,22 @@
+let saludo = "Bienvenidos!!!"
+alert(saludo)
+
+let leyenda = "<h3>Hola mundo desde JavaScript</h3>"
+document.write(leyenda);
+
+
+let nombre1 = "Queenie"
+let nombre2 = "Daryl"
+let nombre3 = "Michonne"
+
+function mostrarNombreDeMichis(){
+    console.log("El nombre del primer michi es: "+nombre1)
+    console.log("El nombre del segundo michi es: "+nombre2)
+    console.log("El nombre del tercero michi es: "+nombre3)
+}
+mostrarNombreDeMichis();
+
+
+
+
+
