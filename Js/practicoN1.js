7d02b3e2efb729bf311c58fc6e3d1290cac8dd1b
Matias <matias.job19@gmail.com>
2023-11-07 21:53:39 -0300

    Practico N1 con Javascript

---
+++ Js/practicoN1.js
@@ -0,0 +1,22 @@
+ // Autor Paz Matias Andres
+
+ console.log("Este es un practico sobre los primeros pasos en JS");
+ 
+ let edad = prompt("Que edad tenes?");
+ 
+ alert("Hola a todos tengo: " + edad + " Años.");
+
+
+ console.log("Estamos por hacer una suma");
+
+let a = prompt("Ingrese el primer número:");
+let b = prompt("Ingrese el primer número:");
+
+  function sumar(){
+    let a = prompt("Ingrese el primer número:");
+    let b = prompt("Ingrese el primer número:");
+
+   let suma = a + b;
+
+   document.writeln(suma);
+  }
