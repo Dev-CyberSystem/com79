db8bcca2d1838be269d4b14e6048e2c0121e1859
Gonza-herrera-xside <gherrera@xsidesolutions.com>
2023-11-08 18:16:23 -0300

    agregue variables y contenidojs

---
+++ agustina carrizo/tp1js.js
@@ -0,0 +1,23 @@
+let saludoDonJose = " hola don pepito!"
+
+let saludoDonPepito = "hola don josé!!"
+
+let preguntaJose = "paso usted ya por casa??"
+
+let respuestaPepito = "por su casa yo pase!!"
+
+let preguntaJoseDos= "vió usted a mi abuela??"
+
+let preguntaPepitoDos = "a su abuela no la vi!!"
+
+let despidoJose= "adióss don pepito!!"
+
+let despidoPepito = "adiosss don jose!!"
+
+let numero1 = 5
+
+let numero2=10
+
+let numero3=16
+
+let resultado = numero1 + numero2 + numero3
