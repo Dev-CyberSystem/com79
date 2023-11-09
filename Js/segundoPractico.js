5cd18eed42666bb9c5c7a858896f91020382fee4
Facundo Gimenez <facuie014@gmail.com>
2023-11-09 01:39:30 -0300

    mitad de TP2

--- Js/segundoPractico.js
+++ Js/segundoPractico.js
@@ -6,5 +6,38 @@ console.log(primerPunto)
 // Crea dos variables con valores numéricos diferentes y compáralas usando el operador de igualdad (==). Muestra el resultado en la consola.
 let segundoPunto1=27
 let segundoPunto2=32
-let comparacion=segundoPunto1==segundoPunto2
-console.log(comparacion)
+let comparacion1=segundoPunto1==segundoPunto2
+console.log(comparacion1)
+
+// Crea dos variables con valores numéricos diferentes y compáralas usando el operador de desigualdad (!=). Muestra el resultado en la consola.
+let tercerPunto1=27
+let tercerPunto2=32
+let comparacion2=tercerPunto1!=tercerPunto2
+console.log(comparacion2)
+
+// Crea una variable con un valor numérico y aumenta su valor en 5 utilizando el operador de incremento (++). Muestra el resultado en la consola.
+let cuartoPunto=10
+cuartoPunto+=5
+console.log(cuartoPunto)
+
+// Crea una variable con un valor numérico y disminuye su valor en 3 utilizando el operador de decremento (--). Muestra el resultado en la consola.
+let quintoPunto=10
+quintoPunto-=3
+console.log(quintoPunto)
+
+// Crea una variable que almacene una cadena de texto y utiliza el operador de concatenación (+) para agregar más texto al final de la cadena. Muestra el resultado en la consola.
+let sextoPunto=`facundo`
+let sextoPunto2=`${sextoPunto} Gimenez`
+console.log(sextoPunto2)
+
+// Crea dos variables con valores booleanos diferentes y utiliza el operador lógico AND (&&) para compararlas. Muestra el resultado en la consola.
+let septimoPunto=10>12
+let septimoPunto2=10==10
+let septimoResultado=septimoPunto&&septimoPunto2
+console.log(septimoResultado)
+// Crea dos variables con valores booleanos diferentes y utiliza el operador lógico OR (||) para compararlas. Muestra el resultado en la consola.
+// Crea una variable que almacene un valor numérico y utiliza el operador de comparación mayor que (>) para compararlo con otro valor numérico. Muestra el resultado en la consola.
+// Crea una variable que almacene un valor numérico y utiliza el operador de comparación menor o igual que (<=) para compararlo con otro valor numérico. Muestra el resultado en la consola.
+// Mediante un prompt pide al usuario que ingrese un numero y luego muestra el resultado en la consola.
+// Mediante un prompt pide al usuario que ingrese su nombre y luego muestra el resultado en la consola.
+// Mediante un prompt pide al usuario que ingrese dos numeros, estos se deben almacenar en dos variables diferentes, luego haz una suma de ambos y que el resultado se guarde en otra variable, muestra el resultado de la suma en el documento.
