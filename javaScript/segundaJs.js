8085348ad264a1142d90ffff0df9525a120d9307
willytucuman <tomasgroso09@hotmail.com>
2023-11-10 02:31:41 -0300

    tp2

--- javaScript/segundaJs.js
+++ javaScript/segundaJs.js
@@ -131,15 +131,58 @@ let posicion = texto.indexOf("m")
     //Practico numero 2
 //Quiz Javascript - Introducción
 //Realiza una operación matemática simple y almacena el resultado en una variable. Luego, muestra el resultado en la consola.
+function sumar(a,b){
+    suma = a+b;
+    console.log(suma)
+}
+sumar(5,19)
 // Crea dos variables con valores numéricos diferentes y compáralas usando el operador de igualdad (==). Muestra el resultado en la consola.
+x= 10;
+y=20;
+console.log(x==y)
 // Crea dos variables con valores numéricos diferentes y compáralas usando el operador de desigualdad (!=). Muestra el resultado en la consola.
+x= 10;
+y=20;
+console.log(x!=y)
 // Crea una variable con un valor numérico y aumenta su valor en 5 utilizando el operador de incremento (++). Muestra el resultado en la consola.
+x=10;
+x+=5;
+console.log(x)
+
 // Crea una variable con un valor numérico y disminuye su valor en 3 utilizando el operador de decremento (--). Muestra el resultado en la consola.
+x=10;
+x-=3
+console.log(x)
+
 // Crea una variable que almacene una cadena de texto y utiliza el operador de concatenación (+) para agregar más texto al final de la cadena. Muestra el resultado en la consola.
+nombre = "tomas"
+saludo = "Hola como estas, "
+frase = nombre+saludo
+console.log(frase)
 // Crea dos variables con valores booleanos diferentes y utiliza el operador lógico AND (&&) para compararlas. Muestra el resultado en la consola.
+verdad = false
+mentira =true 
+console.log(verdad && mentira)
 // Crea dos variables con valores booleanos diferentes y utiliza el operador lógico OR (||) para compararlas. Muestra el resultado en la consola.
+v = false
+m=true 
+console.log(v|| m)
 // Crea una variable que almacene un valor numérico y utiliza el operador de comparación mayor que (>) para compararlo con otro valor numérico. Muestra el resultado en la consola.
+a = 10
+b=12
+(a>b ? console.log("verdad") : console.log("mentira"))
 // Crea una variable que almacene un valor numérico y utiliza el operador de comparación menor o igual que (<=) para compararlo con otro valor numérico. Muestra el resultado en la consola.
+a = 10
+b=12
+(a<=b ? console.log("verdad") : console.log("mentira"))
 // Mediante un prompt pide al usuario que ingrese un numero y luego muestra el resultado en la consola.
+numeroUsuario = prompt("ingresa tu numero favorito: ")
+console.log("tu numero es: "+ numeroUsuario)
 // Mediante un prompt pide al usuario que ingrese su nombre y luego muestra el resultado en la consola.
+nombreUsuario = prompt("Ingresa tu nombre: ")
+console.log(`Hola ${nombreUsuario} como estas,bienvenido`)
 // Mediante un prompt pide al usuario que ingrese dos numeros, estos se deben almacenar en dos variables diferentes, luego haz una suma de ambos y que el resultado se guarde en otra variable, muestra el resultado de la suma en el documento.
+n1 = prompt("Ingresa el primer numero: ")
+n2= prompt("ingresa el segundo numero: ")
+suma = n1+n2;
+document.write(suma)
