fea4557d7e2e30fab5622ae9008abb2e45efe05c
Axel Molina <molinaaxel.agustin1@gmail.com>
2023-11-11 05:30:35 -0300

    Se corrige ruta de archivo JS

---
+++ javaScript/trabajoPractico2.js
@@ -0,0 +1,31 @@
+// Ejercicio JS - Trabajo Practico 2
+/* 1-  Realiza una operación matemática simple y almacena el resultado en una variable. Luego, muestra el resultado en la consola. */ 
+function ejercicio1(){
+    var a = 5;
+    var b = 10;
+    var resultado =  a + b;
+    console.log("El resultado de la suma es: " + resultado);
+}
+
+/* 2 - Crea dos variables con valores numéricos diferentes y compáralas usando el operador de igualdad (==). Muestra el resultado en la consola. */
+function ejercicio2(){
+    var a = 5;
+    var b = 10;
+    var resultado = a == b ;
+    console.log(resultado) ;
+}
+
+/* Crea dos variables con valores numéricos diferentes y compáralas usando el operador de desigualdad (!=). Muestra el resultado en la consola. */
+function ejercicio3(){
+    var a = 5;
+    var b = 10;
+    var resultado = (a != b) ;
+    console.log(resultado) ;
+}
+
+/* Crea una variable con un valor numérico y aumenta su valor en 5 utilizando el operador de incremento (++). Muestra el resultado en la consola. */
+function ejercico4(){
+    var a = 20;
+    a += 5 ;
+    console.log(a);
+}
