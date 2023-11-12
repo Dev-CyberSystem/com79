d97a9eff1cdabfcc67088750bcfa54f9fad6cc6a
jeremiasjm <jeremiasjuarezmarrades15@gmail.com>
2023-11-11 23:36:24 -0300

    creacion de funciones con variables let y const

---
+++ index.js
@@ -0,0 +1,12 @@
+let firt_name = "Soy";
+
+function nombre() {
+  console.log(firt_name);
+}
+function apellido() {
+  const last_name = "Dalto";
+  console.log(last_name);
+}
+
+nombre();
+apellido();
