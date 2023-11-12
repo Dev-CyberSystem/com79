c88fbebda25679788ab0a74a36e41bde7277b44a
Axel Molina <molinaaxel.agustin1@gmail.com>
2023-11-12 20:57:07 -0300

    Se implementa llamado de funciones

--- javaScript/trabajoPractico2.js
+++ javaScript/trabajoPractico2.js
@@ -96,3 +96,18 @@ function ejercicio13(){
     var resultado = numeroA + numeroB;
     console.log(resultado);
 }
+
+// Llamado de funciones
+ejercicio1();
+ejercicio2();
+ejercicio3();
+ejercicio4();
+ejercicio5();
+ejercicio6();
+ejercicio7();
+ejercicio8();
+ejercicio9();
+ejercicio10();
+ejercicio11();
+ejercicio12();
+ejercicio13();
