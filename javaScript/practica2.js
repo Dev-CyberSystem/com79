1eabfbf1b5ce1d31873231572bdcd0130123eb14
Mariana Aguirre <mcargestoria@gmail.com>
2023-11-13 18:07:20 -0300

    Ejercicio nr1 práctica 2

--- javaScript/practica2.js
+++ javaScript/practica2.js
@@ -1,4 +1,14 @@
-function edad ()
-{
-    
-}
+
+//function edad 
+  //  let anioActual = prompt("Ingresá el año actual")
+  //  let nacimiento = prompt ("Ingresá tu año de nacimiento")
+   // let tuEdad = anioActual - nacimiento
+   // document.write(tuEdad)*/
+
+  function edad(){
+    let anioActual= prompt("Ingresá el año actual")
+    let nacimiento= prompt("Ingresá tu año de nacimiento")
+    let tuEdad= anioActual - nacimiento
+    alert("Tenés " + tuEdad + " años")
+  }
+  edad()
