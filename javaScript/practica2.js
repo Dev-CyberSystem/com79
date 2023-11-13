364339b929bd46a0647391d50434dcf86ea3a094
Mariana Aguirre <mcargestoria@gmail.com>
2023-11-13 18:57:48 -0300

    Ejercicio nr 2

--- javaScript/practica2.js
+++ javaScript/practica2.js
@@ -1,14 +1,20 @@
 
-//function edad 
-  //  let anioActual = prompt("Ingresá el año actual")
-  //  let nacimiento = prompt ("Ingresá tu año de nacimiento")
-   // let tuEdad = anioActual - nacimiento
-   // document.write(tuEdad)*/
+//Ejercicio nr 1
 
-  function edad(){
+/*  function edad(){
     let anioActual= prompt("Ingresá el año actual")
     let nacimiento= prompt("Ingresá tu año de nacimiento")
     let tuEdad= anioActual - nacimiento
     alert("Tenés " + tuEdad + " años")
   }
-  edad()
+  edad()*/
+
+// Ejercicio nr 2
+   let num1 = 24
+   let num2 = 45
+   let distintos = num1==num2
+   function sonIguales()
+{
+    console.log(distintos)
+}
+sonIguales()
