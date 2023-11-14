22db32d76951fd16584dac3078244c69a87499e6
Mariana Aguirre <mcargestoria@gmail.com>
2023-11-14 19:35:30 -0300

    Ejercicio nr2 práctico3

--- javaScript/practica3.js
+++ javaScript/practica3.js
@@ -1,16 +1,26 @@
 //Crea una Funcion que reciba dos parametros y los reste, luego muestra el resultado en la consola.
 
-function resta (num1, num2){
+/*function resta (num1, num2){
     console.log(num1-num2)
 }
 
 resta (78,53);
 
 function restar(){
-    let numero3= prompt("Ingrese un número")
+  let numero3= prompt("Ingrese un número")
     let numero4= prompt("Ingrese otro número")
     let resultado5= numero3 - numero4
     console.log(resultado5)
 }
-restar();
+restar();*/
 
+// Crea una funcion donde se declare una variable con el valor 5 y se le sume 10, luego muestra el resultado en la consola.
+
+
+function sumaDiez(){
+    let soloCinco= 5
+    let sumaleACinco= soloCinco+10
+    console.log(sumaleACinco)
+    alert("Tu resultado es "+ sumaleACinco)
+}
+sumaDiez();
