8177899dd8970df4919b83f4cbe697122158bc46
Matias <matias.job19@gmail.com>
2023-11-18 21:12:20 -0300

    Agregando ejercicios resueltos de la primera parte del 2 al 7

--- Js/Practicos/practicoN3.js
+++ Js/Practicos/practicoN3.js
@@ -1,30 +1,30 @@
 /*//Tarea
-// Crea una Funcion que reciba dos parametros y los reste, luego muestra el resultado en la consola.
-// Crea una funcion donde se declare una variable con el valor 5 y se le sume 10, luego muestra el resultado en la consola.
-// Crea una funcion donde se declaren dos variables con valores en string y se concatenen, luego muestra el resultado en la consola.
-// Crea una funcion donde se declaren dos variables con valores booleanos y se compare si son iguales, luego muestra el resultado en la consola.
-// Crea una funcion donde se declaren dos variables con valores booleanos y se compare si son diferentes, luego muestra el resultado en la consola.
-// Crea una funcion donde se declaren dos variables con valores numericos y se compare si el primero es mayor que el segundo, luego muestra el resultado en la consola.
-// Crea un programa que cuente el número de palabras en una frase proporcionada por el usuario. Una palabra se define como cualquier secuencia de caracteres separada por espacios en blanco.
-//Crear una función que tome un número como parámetro y devuelva "Positivo" si es mayor a cero, "Negativo" si es menor a cero o "Cero" si es igual a cero.
-//Crear una función que tome un número como parámetro y devuelva "Par" si es divisible por 2 o "Impar" si no lo es.
-//Crear un programa que pida al usuario un número y luego muestre si es par o impar.
-// Crear un programa que pida al usuario un número y luego muestre si es positivo, negativo o cero.
-// Crear un programa que pida al usuario dos números y luego muestre cuál es el mayor de los dos.
-// Crear un programa que pida al usuario una contraseña y la valide según ciertas condiciones, como la longitud y la presencia de caracteres especiales.
-// Crear un programa que pida al usuario una calificación y luego muestre si es aprobatoria o no (nota mínima para aprobar es 70).
-//Crear una función que tome una cadena como parámetro y devuelva "Es un palíndromo" si la cadena es igual al revés, de lo contrario, devolver "No es un palíndromo".
-// Crear un programa que pida al usuario una letra y luego muestre si es una vocal o una consonante.
-// Crear un programa que pida al usuario un número del 1 al 7 y luego muestre el día de la semana correspondiente.
-// Crear un programa que pida al usuario un número y luego muestre si es primo o no.
-// Crear un programa que pida al usuario su edad y luego muestre si es mayor o menor de edad (18 años).
-// Crear un programa que pida al usuario dos números y luego muestre si su suma es mayor, menor o igual a 100.
-// Crear un programa que pida al usuario tres números y luego muestre cuál es el mayor de los tres.
-// Crear un programa que pida al usuario una letra y luego muestre si es una letra mayúscula o minúscula.
-//Crear una función que tome un número como parámetro y devuelva "Fizz" si es divisible por 3, "Buzz" si es divisible por 5 o "FizzBuzz" si es divisible por ambos. En cualquier otro caso, devolver el número.
-// Crear un programa que pida al usuario un número y luego muestre si es un número perfecto o no. Un número perfecto es aquel que es igual a la suma de sus divisores propios.
-// Crear un programa que pida al usuario un número y luego muestre su descomposición en factores primos.
-// Crear un programa que pida al usuario una fecha (día, mes y año) y luego muestre si es válida o no. Una fecha es válida si cumple con ciertas condiciones, como la cantidad de días en el mes y si es un año bisiesto o no.
+// 1) Crea una Funcion que reciba dos parametros y los reste, luego muestra el resultado en la consola.
+// 2) Crea una funcion donde se declare una variable con el valor 5 y se le sume 10, luego muestra el resultado en la consola.
+// 3) Crea una funcion donde se declaren dos variables con valores en string y se concatenen, luego muestra el resultado en la consola.
+// 4) Crea una funcion donde se declaren dos variables con valores booleanos y se compare si son iguales, luego muestra el resultado en la consola.
+// 5) Crea una funcion donde se declaren dos variables con valores booleanos y se compare si son diferentes, luego muestra el resultado en la consola.
+// 6) Crea una funcion donde se declaren dos variables con valores numericos y se compare si el primero es mayor que el segundo, luego muestra el resultado en la consola.
+// 7) Crea un programa que cuente el número de palabras en una frase proporcionada por el usuario. Una palabra se define como cualquier secuencia de caracteres separada por espacios en blanco.
+//8) Crear una función que tome un número como parámetro y devuelva "Positivo" si es mayor a cero, "Negativo" si es menor a cero o "Cero" si es igual a cero.
+//9) Crear una función que tome un número como parámetro y devuelva "Par" si es divisible por 2 o "Impar" si no lo es.
+// 10) Crear un programa que pida al usuario un número y luego muestre si es par o impar.
+//11) Crear un programa que pida al usuario un número y luego muestre si es positivo, negativo o cero.
+//12) Crear un programa que pida al usuario dos números y luego muestre cuál es el mayor de los dos.
+//13) Crear un programa que pida al usuario una contraseña y la valide según ciertas condiciones, como la longitud y la presencia de caracteres especiales.
+//14) Crear un programa que pida al usuario una calificación y luego muestre si es aprobatoria o no (nota mínima para aprobar es 70).
+//15) Crear una función que tome una cadena como parámetro y devuelva "Es un palíndromo" si la cadena es igual al revés, de lo contrario, devolver "No es un palíndromo".
+// 16) Crear un programa que pida al usuario una letra y luego muestre si es una vocal o una consonante.
+// 17) Crear un programa que pida al usuario un número del 1 al 7 y luego muestre el día de la semana correspondiente.
+// 18) Crear un programa que pida al usuario un número y luego muestre si es primo o no.
+// 20) Crear un programa que pida al usuario su edad y luego muestre si es mayor o menor de edad (18 años).
+// 21) Crear un programa que pida al usuario dos números y luego muestre si su suma es mayor, menor o igual a 100.
+//22)  Crear un programa que pida al usuario tres números y luego muestre cuál es el mayor de los tres.
+//23) Crear un programa que pida al usuario una letra y luego muestre si es una letra mayúscula o minúscula.
+//24) Crear una función que tome un número como parámetro y devuelva "Fizz" si es divisible por 3, "Buzz" si es divisible por 5 o "FizzBuzz" si es divisible por ambos. En cualquier otro caso, devolver el número.
+//25) Crear un programa que pida al usuario un número y luego muestre si es un número perfecto o no. Un número perfecto es aquel que es igual a la suma de sus divisores propios.
+//26) Crear un programa que pida al usuario un número y luego muestre su descomposición en factores primos.
+//27) Crear un programa que pida al usuario una fecha (día, mes y año) y luego muestre si es válida o no. Una fecha es válida si cumple con ciertas condiciones, como la cantidad de días en el mes y si es un año bisiesto o no.
 */
 //Tareas 2 parte
 // 1) Crea un programa que reciba un número del 1 al 12 y muestre el nombre del mes correspondiente.
@@ -60,8 +60,8 @@
 
 
 // Crea una Funcion que reciba dos parametros y los reste, luego muestra el resultado en la consola.
-
-function restar(n1,n2)
+console.log("-------Ejercicio 1-----------")
+/*function restar(n1,n2)
 {
    n1 = parseInt (prompt("Ingrese el primer numero para la resta: "))
    n2 = parseInt(prompt("Ingrese el segundo numero para la resta: "))
@@ -69,4 +69,98 @@ function restar(n1,n2)
    let resta = n1-n2;
    console.log(resta)
 }
-restar()
+restar()*/
+console.log("------------------")
+// 2) Crea una funcion donde se declare una variable con el valor 5 y se le sume 10, luego muestra el resultado en la consola
+
+console.log("-------Ejercicio 2-----------")/*
+let a = 5
+function valorSuma(a)
+{   
+   let sumaV= parseInt(a + 10)
+   console.log(sumaV)
+}
+valorSuma(a)
+*/
+console.log("------------------")
+// 3) Crea una funcion donde se declaren dos variables con valores en string y se concatenen, luego muestra el resultado en la consola.
+
+console.log("-------Ejercicio 3-----------")/*
+let cad1 = "Hola soy de "
+let cad2 = "Tucumán"
+function concaCadena(cad1,cad2)
+{   
+   console.log("La siguiente cadena es la union de ambas: ")
+   console.log(cad1+""+cad2)
+
+}
+concaCadena(cad1,cad2)
+console.log("------------------")
+*/
+// 4) Crea una funcion donde se declaren dos variables con valores booleanos y se compare si son iguales, luego muestra el resultado en la consola.
+
+console.log("-------Ejercicio 4-----------")
+/*
+function compararBool()
+{   let b = true
+   let a = true
+
+   a==b
+   console.log("Son iguales?: ",a===b)
+ /* 
+ Opcion con IF
+   if(a===b)
+   {
+      console.log("Son iguales")
+   }
+   else
+   {
+      console.log("NO son iguales")
+   }
+   console.log("------------------")  */
+/*}
+compararBool()*/
+//5) Crea una funcion donde se declaren dos variables con valores booleanos y se compare si son diferentes, luego muestra el resultado en la consola.
+console.log("-------Ejercicio 5-----------")
+/*
+function compararBool2()
+{   let b = true
+   let a = false
+
+  
+  console.log("Son diferentes?: ",a!==b)
+ 
+ //Opcion con IF
+ /*  if(a!==b)
+   {
+      console.log(" SI son diferentes")
+   }
+   else
+   {
+      console.log("NO son iguales")
+   }*/
+   console.log("------------------")  
+/*}
+compararBool2()*/
+console.log("-------Ejercicio 6-----------")
+// 6) Crea una funcion donde se declaren dos variables con valores numericos y se compare si el primero es mayor que el segundo, luego muestra el resultado en la consola.
+/*
+num1 = parseInt (prompt("Ingrese el primer numero para ser comparado: "))
+num2 = parseInt(prompt("Ingrese el segundo numero para ser comparado: "))
+
+function compararNum(num1,num2)
+{
+      console.log("El primero numero es mayor? ", num1>num2)
+      
+}
+compararNum(num1,num2)
+console.log("------------------")  */
+// 7) Crea un programa que cuente el número de palabras en una frase proporcionada por el usuario. Una palabra se define como cualquier secuencia de caracteres separada por espacios en blanco.
+
+function contarPalabras() {
+	let f = prompt("Ingresa una frase").trim()
+	let p = f.split(" ").length
+	
+   console.log(p)
+}
+contarPalabras()
