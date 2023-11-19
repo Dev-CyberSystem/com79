e6df535bd09bd045f61f3b1c48928294a4526b66
Matias <matias.job19@gmail.com>
2023-11-19 18:32:00 -0300

    Agregando tareas resueltas de la Segunda Parte

--- Js/Practicos/practicoN3SegundaParte.js
+++ Js/Practicos/practicoN3SegundaParte.js
@@ -1,9 +1,9 @@
 //Tareas 2 parte
 // 1) Crea un programa que reciba un número del 1 al 12 y muestre el nombre del mes correspondiente.
-// 2) Crea un programa que reciba un número del 1 al 7 y muestre el día de la semana correspondiente.
-// 3) Crea un programa que reciba un carácter y muestre si es una vocal o una consonante.
+// 2) Crea un programa que reciba un número del 1 al 7 y muestre el día de la semana correspondiente. DUPLICADO EN LA PARTE 1
+// 3) Crea un programa que reciba un carácter y muestre si es una vocal o una consonante. DUPLICADO EN LA PARTE 1
 // 4) Crea un programa que reciba un número del 1 al 5 y muestre el nombre del día de la semana correspondiente a esa posición en un arreglo.
-// 5) Crea un programa que reciba una letra y muestre si es mayúscula o minúscula.
+// 5) Crea un programa que reciba una letra y muestre si es mayúscula o minúscula. 
 // 6) Crea un programa que reciba una nota del 0 al 10 y muestre la calificación correspondiente (suspenso, aprobado, notable, sobresaliente).
 // 7)Imprimir los números pares del 2 al 20 en la consola.
 // 8)Recorrer un string y mostrar en la consola cada letra en mayúsculas.
@@ -29,3 +29,49 @@
 // 28)Dado un array de números, escribir una función que retorne un nuevo array con todos los números del array original menos el último.
 // 29)Dado un array de números, escribir una función que retorne un nuevo array con todos los números del array original menos el primero y el último.
 // 30)Dado un array de números, escribir una función que retorne un nuevo array con todos los números del array original menos el número que se encuentre en la posición X.
+
+// 1) Crea un programa que reciba un número del 1 al 12 y muestre el nombre del mes correspondiente.
+console.log("-------Ejercicio 1-----------")
+/*
+let numero = parseInt(prompt("Ingresar un numero del 1 al 12: "))
+
+function mesC(numero)
+{
+    switch(numero)
+    {
+        case 1: {alert(`Enero`)};break;
+        case 2: {alert(`Febrero`)};break;
+        case 3: {alert(`Marzo`)};break;
+        case 4: {alert(`Abril`)};break;
+        case 5: {alert(`Mayo`)};break;
+        case 6: {alert(`Junio`)};break;
+        case 7: {alert(`Julio`)};break;
+        case 8: {alert(`Agosto`)};break;
+        case 9: {alert(`Septiembre`)};break;
+        case 10: {alert(`Octubre`)};break;
+        case 11: {alert(`Noviembre`)};break;
+        case 12: {alert(`Diciembre`)};break;
+        default: {alert(`Debe ingresar un numero del 1 al 12`)};break;
+        
+    }
+}
+mesC(numero)*/
+console.log("--------------------")
+//4) Crea un programa que reciba un número del 1 al 5 y muestre el nombre del día de la semana correspondiente a esa posición en un arreglo.
+console.log("-------Ejercicio 4-----------")
+/*
+let semanaArray=()=>{
+    let nm3=parseInt(prompt(`ingrese un numero del 1 al 5`))-1
+    let dias=["lunes","martes","miercoles","jueves","viernes"];
+    if(nm3>0&&nm3<6)
+    {
+         {alert(`El es dia ${dias[nm3]}`)}
+    }
+   else{
+    {alert(`Ingresar un numero dentro de 1 a 5.`)}
+   }
+}
+semanaArray()*/
+console.log("--------------------")
+// 5) Crea un programa que reciba una letra y muestre si es mayúscula o minúscula. 
+
