605389495afdf6b5bb0d11783d03979206ae3f28
Matias <matias.job19@gmail.com>
2023-11-18 22:35:52 -0300

    Agregando ejercicios resueltos de la primera parte del 7 al 12

--- Js/Practicos/practicoN3.js
+++ Js/Practicos/practicoN3.js
@@ -17,14 +17,14 @@
 // 16) Crear un programa que pida al usuario una letra y luego muestre si es una vocal o una consonante.
 // 17) Crear un programa que pida al usuario un número del 1 al 7 y luego muestre el día de la semana correspondiente.
 // 18) Crear un programa que pida al usuario un número y luego muestre si es primo o no.
-// 20) Crear un programa que pida al usuario su edad y luego muestre si es mayor o menor de edad (18 años).
-// 21) Crear un programa que pida al usuario dos números y luego muestre si su suma es mayor, menor o igual a 100.
-//22)  Crear un programa que pida al usuario tres números y luego muestre cuál es el mayor de los tres.
-//23) Crear un programa que pida al usuario una letra y luego muestre si es una letra mayúscula o minúscula.
-//24) Crear una función que tome un número como parámetro y devuelva "Fizz" si es divisible por 3, "Buzz" si es divisible por 5 o "FizzBuzz" si es divisible por ambos. En cualquier otro caso, devolver el número.
-//25) Crear un programa que pida al usuario un número y luego muestre si es un número perfecto o no. Un número perfecto es aquel que es igual a la suma de sus divisores propios.
-//26) Crear un programa que pida al usuario un número y luego muestre su descomposición en factores primos.
-//27) Crear un programa que pida al usuario una fecha (día, mes y año) y luego muestre si es válida o no. Una fecha es válida si cumple con ciertas condiciones, como la cantidad de días en el mes y si es un año bisiesto o no.
+// 19) Crear un programa que pida al usuario su edad y luego muestre si es mayor o menor de edad (18 años).
+// 20) Crear un programa que pida al usuario dos números y luego muestre si su suma es mayor, menor o igual a 100.
+//21)  Crear un programa que pida al usuario tres números y luego muestre cuál es el mayor de los tres.
+//22) Crear un programa que pida al usuario una letra y luego muestre si es una letra mayúscula o minúscula.
+//23) Crear una función que tome un número como parámetro y devuelva "Fizz" si es divisible por 3, "Buzz" si es divisible por 5 o "FizzBuzz" si es divisible por ambos. En cualquier otro caso, devolver el número.
+//24) Crear un programa que pida al usuario un número y luego muestre si es un número perfecto o no. Un número perfecto es aquel que es igual a la suma de sus divisores propios.
+//25) Crear un programa que pida al usuario un número y luego muestre su descomposición en factores primos.
+//26) Crear un programa que pida al usuario una fecha (día, mes y año) y luego muestre si es válida o no. Una fecha es válida si cumple con ciertas condiciones, como la cantidad de días en el mes y si es un año bisiesto o no.
 */
 //Tareas 2 parte
 // 1) Crea un programa que reciba un número del 1 al 12 y muestre el nombre del mes correspondiente.
@@ -156,7 +156,8 @@ function compararNum(num1,num2)
 compararNum(num1,num2)
 console.log("------------------")  */
 // 7) Crea un programa que cuente el número de palabras en una frase proporcionada por el usuario. Una palabra se define como cualquier secuencia de caracteres separada por espacios en blanco.
-
+console.log("-------Ejercicio 7-----------")
+/*
 function contarPalabras() {
 	let f = prompt("Ingresa una frase").trim()
 	let p = f.split(" ").length
@@ -164,3 +165,109 @@ function contarPalabras() {
    console.log(p)
 }
 contarPalabras()
+console.log("------------------")*/
+//8) Crear una función que tome un número como parámetro y devuelva "Positivo" si es mayor a cero, "Negativo" si es menor a cero o "Cero" si es igual a cero.
+console.log("-------Ejercicio 8-----------")
+/*
+let num3 = -1
+
+function igualAcero(num3)
+{
+      if(num3>0)
+      {
+         console.log("Es mayor a 0: ")
+      }
+      else if(num3<0)
+      {
+         console.log("Es menor a 0: ")
+      }
+      else 
+      console.log("Es igual a 0: ")
+}
+igualAcero(num3)*/
+console.log("------------------")
+
+//9) Crear una función que tome un número como parámetro y devuelva "Par" si es divisible por 2 o "Impar" si no lo es.
+console.log("-------Ejercicio 9-----------")
+/*
+let num5 = 10
+function parOimpar2(num5){
+
+   if (num5 % 2 === 0) {
+       console. log(`El numero ${num5} es Par`);
+      }
+   else
+   {
+       console. log(`El numero ${num5} es Impar`);
+   }
+}
+parOimpar2(num5)*/
+console.log("------------------")
+// 10) Crear un programa que pida al usuario un número y luego muestre si es par o impar.
+console.log("-------Ejercicio 10-----------")
+/*let num4 = parseInt(prompt("Ingrese un numero para saber si es par o impar: "))
+
+function parOimpar(num4){
+
+   if (num4 % 2 === 0) {
+       console. log(`El numero ${num4} es Par`);
+      }
+   else
+   {
+       console. log(`El numero ${num4} es Impar`);
+   }
+}
+parOimpar(num4)*/
+console.log("------------------")
+//11) Crear un programa que pida al usuario un número y luego muestre si es positivo, negativo o cero.
+console.log("-------Ejercicio 11-----------")
+/*
+let numb = parseInt(prompt("Ingrese un numero positivo o negativo para ser comparado: "))
+
+function igualAcero(numb)
+{
+      if(numb!==0)
+      { 
+         switch(numb)
+         {
+            case 1: 
+            console.log(`El numero ${numb} es mayor a 0 `);
+               break;
+            default:
+               console.log(`El numero ${numb} es menor a 0 `);
+             break; 
+            } 
+         }
+      else{
+          console.log(`El numero ${numb} es igual 0 `);
+      }          
+}
+igualAcero(numb)*/
+console.log("------------------")
+
+//12) Crear un programa que pida al usuario dos números y luego muestre cuál es el mayor de los dos.
+console.log("-------Ejercicio 12-----------")
+function numM(){
+   let nu1 = parseInt(prompt("Ingrese el primer numero: "))
+   let nu2 = parseInt(prompt("Ingrese el segundo numero: "))
+  
+   if(nu1===0 && nu2===0)
+   {
+      console.log(`El numero ${nu2} y ${nu1} son iguales a 0 `);
+   }
+   else if(nu1>nu2)
+   {
+      console.log(`El numero ${nu1} es mayor a ${nu2} `);
+   }
+   else
+   {
+      console.log(`El numero ${nu2} es mayor a ${nu1} `);
+   }
+   
+}
+numM()
+console.log("------------------")
+//13) Crear un programa que pida al usuario una contraseña y la valide según ciertas condiciones, como la longitud y la presencia de caracteres especiales.
+//14) Crear un programa que pida al usuario una calificación y luego muestre si es aprobatoria o no (nota mínima para aprobar es 70).
+//15) Crear una función que tome una cadena como parámetro y devuelva "Es un palíndromo" si la cadena es igual al revés, de lo contrario, devolver "No es un palíndromo".
+// 16) Crear un programa que pida al usuario una letra y luego muestre si es una vocal o una consonante.
