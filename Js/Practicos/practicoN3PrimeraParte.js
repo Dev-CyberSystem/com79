a2ba63b1007808a93bdcce9d35f59226e81296e8
Matias <matias.job19@gmail.com>
2023-11-19 12:38:37 -0300

    Agregando tareas del 17 al 17 primera parte

--- Js/Practicos/practicoN3PrimeraParte.js
+++ Js/Practicos/practicoN3PrimeraParte.js
@@ -26,37 +26,7 @@
 //25) Crear un programa que pida al usuario un número y luego muestre su descomposición en factores primos.
 //26) Crear un programa que pida al usuario una fecha (día, mes y año) y luego muestre si es válida o no. Una fecha es válida si cumple con ciertas condiciones, como la cantidad de días en el mes y si es un año bisiesto o no.
 */
-//Tareas 2 parte
-// 1) Crea un programa que reciba un número del 1 al 12 y muestre el nombre del mes correspondiente.
-// 2) Crea un programa que reciba un número del 1 al 7 y muestre el día de la semana correspondiente.
-// 3) Crea un programa que reciba un carácter y muestre si es una vocal o una consonante.
-// 4) Crea un programa que reciba un número del 1 al 5 y muestre el nombre del día de la semana correspondiente a esa posición en un arreglo.
-// 5) Crea un programa que reciba una letra y muestre si es mayúscula o minúscula.
-// 6) Crea un programa que reciba una nota del 0 al 10 y muestre la calificación correspondiente (suspenso, aprobado, notable, sobresaliente).
-// 7)Imprimir los números pares del 2 al 20 en la consola.
-// 8)Recorrer un string y mostrar en la consola cada letra en mayúsculas.
-// 9)Pedir al usuario que ingrese un número y mostrar en la consola los números del 1 al número ingresado.
-// 10)Pedir al usuario que ingrese una serie de números separados por coma y mostrar en la consola el número más grande de la serie.
-// 11)Pedir al usuario que ingrese una palabra y mostrar en la consola si la palabra es un palíndromo o no.
-// 12)Pedir al usuario que ingrese una serie de números separados por coma y mostrar en la consola la suma de los números pares.
-// 13)Pedir al usuario que ingrese una serie de números separados por coma y mostrar en la consola la cantidad de números negativos.
-// 14)Pedir al usuario que ingrese un número y mostrar en la consola todos los números impares desde el 1 hasta el número ingresado.
-// 15)Pedir al usuario que ingrese una serie de números separados por coma y mostrar en la consola el número más pequeño de la serie.
-// 16)Pedir al usuario que adivine un número generado aleatoriamente entre 1 y 100. Mostrar en la consola si el usuario adivinó o no el número y la cantidad de intentos que le tomó.
-// 17)Pedir al usuario que ingrese una serie de números separados por coma y mostrar en la consola la cantidad de números pares.
-// 18)Dado un array de números, escribir una función que retorne el número más grande del array.
-// 19)Dado un array de números, escribir una función que retorne un nuevo array con los números pares del array original.
-// 20)Dado un array de números, escribir una función que retorne la suma de todos los números del array.
-// 21)Dado un array de strings, escribir una función que retorne un nuevo array con todos los strings en mayúsculas.
-// 22)Dado un array de números y un número X, escribir una función que retorne un nuevo array con todos los números del array original que sean mayores que X.
-// 23)Dado un array de números, escribir una función que retorne la suma de todos los números del array. Además, si algún número es mayor que 10, agregarlo a un nuevo array de "números grandes".
-// 24)Dado un array de strings, escribir una función que retorne un nuevo array con todos los strings que tengan más de 5 caracteres.
-// 25)Dado un array de strings, escribir una función que retorne un nuevo array con todos los strings que tengan más de 5 caracteres y que empiecen con la letra "a".
-// 26)Dado un array de números y un número X, escribir una función que retorne un nuevo array con todos los números del array original que sean mayores que X. Además, cortar el array resultante para que tenga solamente los primeros 3 números.
-// 27)Dado un array de números, escribir una función que retorne un nuevo array con todos los números del array original, pero sumándoles 1.
-// 28)Dado un array de números, escribir una función que retorne un nuevo array con todos los números del array original menos el último.
-// 29)Dado un array de números, escribir una función que retorne un nuevo array con todos los números del array original menos el primero y el último.
-// 30)Dado un array de números, escribir una función que retorne un nuevo array con todos los números del array original menos el número que se encuentre en la posición X.
+
 
 
 // Crea una Funcion que reciba dos parametros y los reste, luego muestra el resultado en la consola.
@@ -311,7 +281,7 @@ function calificacion(califica)
 }
 calificacion(califica)
 */
-console.log("--------------------------")
+console.log("-----------------------")
 
 
 //15) Crear una función que tome una cadena como parámetro y devuelva "Es un palíndromo" si la cadena es igual al revés, de lo contrario, devolver "No es un palíndromo".
@@ -331,10 +301,67 @@ function palidromo1(cadena1){
    }
 }
 palidromo1(cadena1)*/
-console.log("--------------------------")
+console.log("----------------------")
 // 16) Crear un programa que pida al usuario una letra y luego muestre si es una vocal o una consonante.
+console.log("-------Ejercicio 16-----------")
+/*
+let letra = String(prompt("Ingres una letra: "))
+
+function vocalCons(letra){
+
+   if(letra.length === 1)
+   {
+      switch(letra)
+   {
+      case `a`:{ console.log(`Es una vocal ${letra}`); break;}
+      case `e`: {console.log(`Es una vocal ${letra}`); break;}
+      case `i`:{console.log(`Es una vocal ${letra}`); break;}
+      case `o`: {console.log(`Es una vocal ${letra}`); break;}
+      case `u`: {console.log(`Es una vocal ${letra}`); break;}
+         case `A`:{ console.log(`Es una vocal ${letra}`) ; break;}
+         case `E`: {console.log(`Es una vocal ${letra}`); break;}
+         case `I`:{console.log(`Es una vocal ${letra}`); break;}
+         case `O`: {console.log(`Es una vocal ${letra}`); break;}
+         case `U`: {console.log(`Es una vocal ${letra}`); break;}
+    default:
+      {
+         console.log(`Es una consonante ${letra}`)
+         break;
+      }
+   }
+   }
+   else{
+      console.log("No ingreso una letra..-")
+   }
+
+}
+vocalCons(letra)*/
+console.log("----------------------")
+
 // 17) Crear un programa que pida al usuario un número del 1 al 7 y luego muestre el día de la semana correspondiente.
+console.log("-------Ejercicio 17-----------")
+/*
+let n7 = parseInt(prompt("Ingrese un numero del 1 al 7: "))
+
+function semanaC(n7)
+{
+      switch (n7) {
+         case 1:{console.log("Lunes")} ;break;
+         case 2:{console.log("Martes")} ;break;
+         case 3:{console.log("Miercoles")} ;break;
+         case 4:{console.log("Jueves")} ;break;
+         case 5:{console.log("Viernes")} ;break;
+         case 6:{console.log("Sabados")} ;break;
+         case 7:{console.log("Domingos")} ;break;
+         default:
+            console.log("El numero ingresado no corresponde al dia de la semana.")
+            break;
+      }
+}
+semanaC(n7)*/
+console.log("-----------------------")
 // 18) Crear un programa que pida al usuario un número y luego muestre si es primo o no.
+
 // 19) Crear un programa que pida al usuario su edad y luego muestre si es mayor o menor de edad (18 años).
 // 20) Crear un programa que pida al usuario dos números y luego muestre si su suma es mayor, menor o igual a 100.
 //21)  Crear un programa que pida al usuario tres números y luego muestre cuál es el mayor de los tres.
