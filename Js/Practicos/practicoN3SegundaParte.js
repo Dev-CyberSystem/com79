e496837f10ab1b9e0d80358c2a31662f9e700833
Matias <matias.job19@gmail.com>
2023-11-20 20:55:01 -0300

    tareas resueltas hasta el 25 de la Segunda Parte

--- Js/Practicos/practicoN3SegundaParte.js
+++ Js/Practicos/practicoN3SegundaParte.js
@@ -450,9 +450,31 @@ function arrayCadena5A()
 arrayCadena5A()
 */
 console.log("--------------------")
-// 26)Dado un array de números y un número X, escribir una función que retorne un nuevo array con todos los números del array original que sean mayores que X. Además, cortar el array resultante para que tenga solamente los primeros 3 números.
+// 26)Dado un array de números y un número X, escribir una función que retorne un nuevo array con todos los números del array original que sean mayores que X. 
+//Además, cortar el array resultante para que tenga solamente los primeros 3 números.
 console.log("-------Ejercicio 26-----------")
+/*
+let arrayNuevo = [0,23,2,21,4,20,3,25,31,33,45,67,85,3,1,7,8,9,88,44,20,2020]
+let numXX= 43
 
+function arrayCortado3()
+{
+        let arrayOr2 =[]
+    
+        for (let i = 0; i< arrayNuevo.length; i++) 
+        {
+            if(arrayNuevo[i]>numXX)
+            {
+                arrayOr2.push(arrayNuevo[i])
+                if (arrayOr2.length>3) {
+                    arrayOr2.pop()
+                }
+            }
+        }
+        alert(`[${arrayOr2}]`)
+     
+}
+arrayCortado3()*/
 console.log("--------------------")
 // 27)Dado un array de números, escribir una función que retorne un nuevo array con todos los números del array original, pero sumándoles 1.
 // 28)Dado un array de números, escribir una función que retorne un nuevo array con todos los números del array original menos el último.
