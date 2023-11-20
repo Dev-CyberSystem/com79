21574b832dea9569e12b80d5fee81c3de637cbdb
Santiago Romero <santiagoromero0205@gmail.com>
2023-11-20 20:45:06 -0300

    meses de anio

--- javasScriptTP/cuartoPracticoSantiRomero.js
+++ javasScriptTP/cuartoPracticoSantiRomero.js
@@ -462,7 +462,7 @@ function mostrarDescomposicion() {
 mostrarDescomposicion();*/
 
 //anio bisiesto
-function esFechaValida(dia, mes, anio) {
+/*function esFechaValida(dia, mes, anio) {
 
   const esBisiesto = (anio % 4 === 0 && anio % 100 !== 0) || (anio % 400 === 0);
 
@@ -487,4 +487,53 @@ function verificarFecha() {
   }
 }
 
-verificarFecha();
+verificarFecha();*/
+ 
+//meses del anio
+
+let mesesDelAnio = parseInt(prompt("ingrese un número del 1 al 12"))
+
+switch (mesesDelAnio) {
+  case 1: 
+  mes = "Enero";
+  break;
+  case 2: 
+  mes = "Febrero";
+  break;
+  case 3: 
+  mes = "Marzo";
+  break;
+  case 4:
+  mes = "Abril";
+  break;
+  case 5: 
+  mes = "Mayo";
+  break
+  case 6: 
+  mes = "Junio";
+  break;
+  case 7: 
+  mes = "Julio";
+  break;
+  case 8: 
+  mes = "Agosto";
+  break;
+  case 9:   
+  mes = "Septiembre";
+  break;
+  case 10:  
+  mes = "Octubre";
+  break;
+  case 11: 
+  mes = "Noviembre";
+  break;
+  case 12: 
+  mes = "Diciembre";
+  break;
+  default:
+    mes = "inexistente";
+    break;
+}
+ console.log(`El número ${mesesDelAnio} es el mes ${mes}`)
+
+
