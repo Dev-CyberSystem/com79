61f78542c7a7cffc5acd5fe26d51868f9e443f56
Mariana Aguirre <mcargestoria@gmail.com>
2023-11-20 00:11:15 -0300

    Ejercicio 4 práctica4

--- javaScript/practica4.js
+++ javaScript/practica4.js
@@ -10,12 +10,45 @@ mesesAnio()*/
 // []
 // 2) Crea un programa que reciba un número del 1 al 7 y muestre el día de la semana correspondiente.
 
- function semana(){
+/* function semana(){
     let diaSemana= ["", "Hoy es Lunes", "Hoy es Martes", "Hoy es Miércoles", "Hoy es Jueves", "Hoy es Viernes", "Hoy es Sábado", "Hoy es Domingo"];
     let elegiDia= diaSemana.length;
     let queDiaEs= diaSemana[prompt("Escribe un número del 1 al 7")];
     console.log(queDiaEs);
  }
  semana();
+ */
 
- 
+ // 3) Crea un programa que reciba un carácter y muestre si es una vocal o una consonante.
+// []
+// ||
+
+
+ /*function abecedario() {
+    let letraSeleccionada= prompt("Escribe una letra del abecedario");
+    let vocales= [];
+  if ()
+    {
+    console.log("La letra que ingresaste es una vocal")}
+    else {
+        console.log("La letra que ingresaste es una consonante")
+    }
+
+ }
+abecedario();*/
+
+// 4) Crea un programa que reciba un número del 1 al 5 y muestre el nombre del día de la semana correspondiente a esa posición en un arreglo.
+// []
+// ||
+/*function diasLaborales(){
+    let dias = ["", "Lunes", "Martes", "Miércoles", "Jueves" , "Viernes"];
+    let seleccionDia= dias.length;
+    let diaTrabajo= dias[prompt("Ingrese un número del 1 al 5")];
+ console.log("Hoy es " + diaTrabajo);
+
+}
+diasLaborales()*/
+
+// 5) Crea un programa que reciba una letra y muestre si es mayúscula o minúscula.
+// []
+// ||
