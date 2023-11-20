d53b08b63d662faaa239c94edf60dbc01c6f2302
Mariana Aguirre <mcargestoria@gmail.com>
2023-11-19 21:44:21 -0300

    Ejercicio 2 práctica4

--- javaScript/practica4.js
+++ javaScript/practica4.js
@@ -1,9 +1,21 @@
 // 1) Crea un programa que reciba un número del 1 al 12 y muestre el nombre del mes correspondiente.
 
-function mesesAnio() {
+/*function mesesAnio() {
 let meses =  [" ","Enero", "Febrero", "Marzo", "Abril","Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
 let elegiMes= meses.length;
 let posicion = meses[prompt("Escribí un número del 1 al 12")];
 console.log(posicion);
 }
-mesesAnio()
+mesesAnio()*/
+// []
+// 2) Crea un programa que reciba un número del 1 al 7 y muestre el día de la semana correspondiente.
+
+ function semana(){
+    let diaSemana= ["", "Hoy es Lunes", "Hoy es Martes", "Hoy es Miércoles", "Hoy es Jueves", "Hoy es Viernes", "Hoy es Sábado", "Hoy es Domingo"];
+    let elegiDia= diaSemana.length;
+    let queDiaEs= diaSemana[prompt("Escribe un número del 1 al 7")];
+    console.log(queDiaEs);
+ }
+ semana();
+
+ 
