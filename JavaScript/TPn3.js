07e8a37fdb5ed13b9a7d55f6d0342d2a021f2a2d
FedericoFrau <fedeafrau@gmail.com>
2023-11-22 00:28:51 -0300

    ejercicio 24, corrección ejercicio 13

--- JavaScript/TPn3.js
+++ JavaScript/TPn3.js
@@ -157,8 +157,7 @@ evenOdd();
 // let stdChain = userInChain.split("");
 // let invChain = [...stdChain].reverse();
 // let validationChain = stdChain.join("")===invChain.join("");
-// validationChain ? alert(`"${userInChain}" Es un palíndromo`) : alert(`"${userInChain}" No es un palíndromo`);
-// console.log(`Resultado ejercicio 13 (palíndromo): ${validationChain}`);
+// validationChain ? (alert(`"${userInChain}" Es un palíndromo`),console.log(`Resultado ejercicio 13 (palíndromo):"${userInChain}" Es un palíndromo`)) : (alert(`"${userInChain}" No es un palíndromo`), console.log(`Resultado ejercicio 13 (palíndromo):"${userInChain}" No es un palíndromo`));
 
 
 // Crear un programa que pida al usuario una letra y luego muestre si es una vocal o una consonante.
@@ -196,7 +195,7 @@ evenOdd();
 // console.log(`Resultado ejercicio 15 (seleccion día): ${selectedDay}`);
 
 
-// Crear un programa que pida al usuario un número y luego muestre si es primo o no.
+// Crear un programa que pida al usuario un número y luego muestre si es primo o no. TODAVIA NO LO PUEDO RESOLVER!!
 
 // Crear un programa que pida al usuario su edad y luego muestre si es mayor o menor de edad (18 años).
 // let adultCheck = () => {
@@ -332,7 +331,99 @@ console.log(`Resultado ejercicio 21 (Fizz Buzz): ${fizzBuzz()}`)
 
 
 // Crear un programa que pida al usuario un número y luego muestre si es un número perfecto o no. Un número perfecto es aquel que es igual a la suma de sus divisores propios.
+// let checkedNumP;
+// let perfectState;
+// let checkInNum = () => {
+//     let userInNumP = prompt(`Por favor ingrese un número:`).trim();
+//     if (userInNumP.match(/[0-9]/)) {
+//         checkedNumP = parseInt(userInNumP);
+//     } else {
+//         alert(`Por favor ingrese solo números`);
+//         checkInNum();
+//     }
+// }
+// checkInNum();
+// let perfectNum = (checkedNumP) => {
+//     if (checkedNumP===)
+// } TODAVÍA NO LO PUEDO TERMINAR DE RESOLVER!!
 
-// Crear un programa que pida al usuario un número y luego muestre su descomposición en factores primos.
+
+// Crear un programa que pida al usuario un número y luego muestre su descomposición en factores primos. TODAVÍA NO LO PUEDO RESOLVER!!
 
 // Crear un programa que pida al usuario una fecha (día, mes y año) y luego muestre si es válida o no. Una fecha es válida si cumple con ciertas condiciones, como la cantidad de días en el mes y si es un año bisiesto o no.
+// let userInDate = prompt(`Por favor introduzca una fecha en formato dd/mm/aaaa:`).split("/");
+// let dayValue = parseInt(userInDate[0].trim());
+// let monthValue = parseInt(userInDate[1].trim());
+// let yearValue = parseInt(userInDate[2].trim());
+// let isYearLeap = yearValue%4===0 && (yearValue%100!==0 || yearValue%400===0);
+// const regularYear = {
+//     1: 31,
+//     2: 28,
+//     3: 31,
+//     4: 30,
+//     5: 31,
+//     6: 30,
+//     7: 31,
+//     8: 31,
+//     9: 30,
+//     10: 31,
+//     11: 30,
+//     12: 31,
+// };
+// const leapYear = {
+//     1: 31,
+//     2: 29,
+//     3: 31,
+//     4: 30,
+//     5: 31,
+//     6: 30,
+//     7: 31,
+//     8: 31,
+//     9: 30,
+//     10: 31,
+//     11: 30,
+//     12: 31,
+// }
+// let validatorDate = () => {
+//     if ((isYearLeap && dayValue<=leapYear[monthValue] && String(yearValue).length<=4) || (!isYearLeap && dayValue<=regularYear[monthValue] && String(yearValue).length<=4)) {
+//         console.log(`Resultado ejercicio 24 (fecha válida): Fecha válida (${dayValue}/${monthValue}/${yearValue})`);
+//         alert(`La fecha ingresada es válida!`);
+//     } else {
+//         console.log(`Resultado ejercicio 24 (fecha válida): Fecha inválida (${dayValue}/${monthValue}/${yearValue})`);
+//         alert(`La fecha ingresada es inválida, por favor intente nuevamente`);
+//     }
+// }
+// validatorDate();
+
+
+
+// 1) Crea un programa que reciba un número del 1 al 12 y muestre el nombre del mes correspondiente.
+// 2) Crea un programa que reciba un número del 1 al 7 y muestre el día de la semana correspondiente.
+// 3) Crea un programa que reciba un carácter y muestre si es una vocal o una consonante.
+// 4) Crea un programa que reciba un número del 1 al 5 y muestre el nombre del día de la semana correspondiente a esa posición en un arreglo.
+// 5) Crea un programa que reciba una letra y muestre si es mayúscula o minúscula.
+// 6) Crea un programa que reciba una nota del 0 al 10 y muestre la calificación correspondiente (suspenso, aprobado, notable, sobresaliente).
+// Imprimir los números pares del 2 al 20 en la consola.
+// Recorrer un string y mostrar en la consola cada letra en mayúsculas.
+// Pedir al usuario que ingrese un número y mostrar en la consola los números del 1 al número ingresado.
+// Pedir al usuario que ingrese una serie de números separados por coma y mostrar en la consola el número más grande de la serie.
+// Pedir al usuario que ingrese una palabra y mostrar en la consola si la palabra es un palíndromo o no.
+// Pedir al usuario que ingrese una serie de números separados por coma y mostrar en la consola la suma de los números pares.
+// Pedir al usuario que ingrese una serie de números separados por coma y mostrar en la consola la cantidad de números negativos.
+// Pedir al usuario que ingrese un número y mostrar en la consola todos los números impares desde el 1 hasta el número ingresado.
+// Pedir al usuario que ingrese una serie de números separados por coma y mostrar en la consola el número más pequeño de la serie.
+// Pedir al usuario que adivine un número generado aleatoriamente entre 1 y 100. Mostrar en la consola si el usuario adivinó o no el número y la cantidad de intentos que le tomó.
+// Pedir al usuario que ingrese una serie de números separados por coma y mostrar en la consola la cantidad de números pares.
+// Dado un array de números, escribir una función que retorne el número más grande del array.
+// Dado un array de números, escribir una función que retorne un nuevo array con los números pares del array original.
+// Dado un array de números, escribir una función que retorne la suma de todos los números del array.
+// Dado un array de strings, escribir una función que retorne un nuevo array con todos los strings en mayúsculas.
+// Dado un array de números y un número X, escribir una función que retorne un nuevo array con todos los números del array original que sean mayores que X.
+// Dado un array de números, escribir una función que retorne la suma de todos los números del array. Además, si algún número es mayor que 10, agregarlo a un nuevo array de "números grandes".
+// Dado un array de strings, escribir una función que retorne un nuevo array con todos los strings que tengan más de 5 caracteres.
+// Dado un array de strings, escribir una función que retorne un nuevo array con todos los strings que tengan más de 5 caracteres y que empiecen con la letra "a".
+// Dado un array de números y un número X, escribir una función que retorne un nuevo array con todos los números del array original que sean mayores que X. Además, cortar el array resultante para que tenga solamente los primeros 3 números.
+// Dado un array de números, escribir una función que retorne un nuevo array con todos los números del array original, pero sumándoles 1.
+// Dado un array de números, escribir una función que retorne un nuevo array con todos los números del array original menos el último.
+// Dado un array de números, escribir una función que retorne un nuevo array con todos los números del array original menos el primero y el último.
+// Dado un array de números, escribir una función que retorne un nuevo array con todos los números del array original menos el número que se encuentre en la posición X.
