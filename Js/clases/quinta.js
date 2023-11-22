ab5ab3f02af1855a5fee5e1098dede458296a9a7
Matias <matias.job19@gmail.com>
2023-11-22 19:49:43 -0300

    Quinta clase js

---
+++ Js/clases/quinta.js
@@ -0,0 +1,23 @@
+//Clase de practica
+
+//1.- Crear un repo en github
+//2.- Crear la rama dev
+//3.- Crear una rama por cada integrante del equipo
+//4.- Vamos a tener solo un archivo index.html y un archivo script.js
+//5.-Mergeamos las branch a dev
+
+//Trabajo en equipo
+//Carrito de productos
+//Carrito de compras con Array
+//Crear una aplicacion de carrito de compras donde se ingresen
+//productos como elementos en un array
+//las tareas que debe hacer son las siguientes:
+//Agregar producto al carrito
+//Listar los productos del carrito
+//Buscar un producto en el carrito
+//Filtrar productos que coincidan con una palabra, parte de ella 
+//una letra
+//Eliminar producto del carrito
+
+//Lunes 5 de febrero PROYECTO JS
+
