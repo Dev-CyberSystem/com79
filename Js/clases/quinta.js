9dcf0edf1bcab1fb2d24a01dae305f919b2d516a
Matias <matias.job19@gmail.com>
2023-11-27 19:14:06 -0300

    Quinta clase js y comienzo de la sexta clase

--- Js/clases/quinta.js
+++ Js/clases/quinta.js
@@ -21,3 +21,25 @@
 
 //Lunes 5 de febrero PROYECTO JS
 
+const productos = ['television', 'celular', 'tablet', 'reloj'];
+let carrito = [];
+
+const ingresarProductos = (productos) => {
+  productos.forEach((producto) => carrito.push(producto));
+
+  console.log(carrito);
+  listarProductos(carrito);
+};
+
+const listarProductos = (carrito) => {
+  for (let i = 0; i < carrito.length; i++) {
+    console.log(carrito[i]);
+  }
+};
+
+//const buscarProducto = ()=> {
+    
+  //  const nuevoCarrito = carrito.map((producto)=>  )
+//}
+
+ingresarProductos(productos)
