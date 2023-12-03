908a5069f7178a8b12c1a919f4d3892190f4614d
Matias <matias.job19@gmail.com>
2023-12-03 11:36:15 -0300

    agregando la ultima clase

--- Js/clases/carrito.js
+++ Js/clases/carrito.js
@@ -1,15 +1,22 @@
-let productos  = []
+let carrito = []
 
-
-const addFromProductos = document.getElementById(`formAddProducts`)
-const addInputProductos = document.getElementById(`inputAddProducts`)
-const addBtnProductos = document.getElementById(`buttonAddProductos`)
-
+const addFormProducts = document.getElementById('formAddProductos')
+const addInputProducts = document.getElementById('inputAddProductos')
+const addBtnProducts = document.getElementById('buttonAddProductos')
+const productList = document.getElementById('productList')
 
 addBtnProductos.addEventListener('click', (event) => {
     event.preventDefault()
+    // verificamos que el input tenga una entrada 
     let producto = addInputProductos.value
-    productos.push(producto)
-    console.log(producto)
-    
+    carrito.push(producto)
+    console.log(carrito, "<--- Soy el Carrito")
+    addFormProductos.reset()
+    listarProductos()
 })
+
+function listarProductos()
+{   
+    console.log()
+
+}
