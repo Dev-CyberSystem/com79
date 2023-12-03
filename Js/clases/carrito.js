abf15f7078f295a60603d33453a3212de62061fa
Matias <matias.job19@gmail.com>
2023-12-03 12:08:12 -0300

    Continuando agregando cambios para el TP5

--- Js/clases/carrito.js
+++ Js/clases/carrito.js
@@ -5,18 +5,33 @@ const addInputProducts = document.getElementById('inputAddProductos')
 const addBtnProducts = document.getElementById('buttonAddProductos')
 const productList = document.getElementById('productList')
 
+
 addBtnProductos.addEventListener('click', (event) => {
     event.preventDefault()
     // verificamos que el input tenga una entrada 
-    let producto = addInputProductos.value
+    let producto = addInputProducts.value.toLowerCase().trim();
     carrito.push(producto)
     console.log(carrito, "<--- Soy el Carrito")
     addFormProductos.reset()
     listarProductos()
-})
+});
 
 function listarProductos()
 {   
-    console.log()
+    productList.innerHTML = "" // para resetear el productoList
+
+    if(carrito.length !== 0 )
+    {
+        carrito.forEach((producto, index) => {
+            const li = document.createElement("li");
+            li.textContent = `${index + 1} + ${producto}`;
+            productList.appendChild(li);
+          });
+    }
+    else{
+        productList.textContent(`El carrito esta vacio`)
+        alert(`No hay producto en el carrito`);
+    }
+ 
 
 }
