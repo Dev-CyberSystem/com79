65778a9331bc6247ef1011fa74b82cd632dd3fc7
Matias <matias.job19@gmail.com>
2023-12-03 13:37:37 -0300

    Continuando agregando cambios para el TP5

--- Js/clases/carrito.js
+++ Js/clases/carrito.js
@@ -1,37 +1,33 @@
-let carrito = []
+let carrito = [];
 
-const addFormProducts = document.getElementById('formAddProductos')
-const addInputProducts = document.getElementById('inputAddProductos')
-const addBtnProducts = document.getElementById('buttonAddProductos')
-const productList = document.getElementById('productList')
+const addFormProducts = document.getElementById('formAddProducts');
+const addInputProducts = document.getElementById('inputAddProducts');
+const addBtnProducts = document.getElementById('buttonAddProducts');
+const productList = document.getElementById('productList');
 
 
-addBtnProductos.addEventListener('click', (event) => {
+addBtnProducts.addEventListener("click", (event) => {
     event.preventDefault()
     // verificamos que el input tenga una entrada 
     let producto = addInputProducts.value.toLowerCase().trim();
-    carrito.push(producto)
-    console.log(carrito, "<--- Soy el Carrito")
-    addFormProductos.reset()
-    listarProductos()
+    carrito.push(producto);
+    console.log(carrito, "<--- Soy el Carrito");
+   // document.getElementById('formAddProductos').reset()
+   addFormProducts.reset();
+   listarProductos();
 });
 
-function listarProductos()
-{   
-    productList.innerHTML = "" // para resetear el productoList
-
-    if(carrito.length !== 0 )
-    {
-        carrito.forEach((producto, index) => {
-            const li = document.createElement("li");
-            li.textContent = `${index + 1} + ${producto}`;
-            productList.appendChild(li);
-          });
+function listarProductos() {
+    productList.innerHTML = "";
+  
+    if (carrito.length !== 0) {
+      carrito.forEach((producto, index) => {
+        const li = document.createElement("li");
+        li.textContent = `${index + 1} - ${producto}`;
+        productList.appendChild(li);
+      });
+    } else {
+      productList.textContent = "El carrito esta Vacio";
+      alert("No hay productos en el carrito");
     }
-    else{
-        productList.textContent(`El carrito esta vacio`)
-        alert(`No hay producto en el carrito`);
-    }
- 
-
-}
+  }
