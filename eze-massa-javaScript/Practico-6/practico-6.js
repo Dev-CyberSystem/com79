bcceaffe866243d313c962c2882e62e360c12726
Ezequiel Massa <ezequielmassa.dev@gmail.com>
2023-12-06 08:12:31 -0300

    feat/ resuelta consigna 3 practico 6

--- eze-massa-javaScript/Practico-6/practico-6.js
+++ eze-massa-javaScript/Practico-6/practico-6.js
@@ -27,3 +27,15 @@ const rectangulo1 = new Rectangulo(200, 10)
 console.log(rectangulo1.calcularArea())
 
 // Crear una clase Persona que tenga las propiedades nombre, edad y profesión. La clase debe tener un método presentarse que imprima en la consola un mensaje con los datos de la persona.
+class Persona {
+	constructor(nombre, edad, profesion) {
+		this.nombre = nombre
+		this.edad = edad
+		this.profesion = profesion
+	}
+	presentarse = () => {
+		return `Hola me llamo ${this.nombre}, tengo ${this.edad} años y mi profesion es ${this.profesion}`
+	}
+}
+const persona1 = new Persona('julio', 34, 'carpintero')
+console.log(persona1.presentarse())
