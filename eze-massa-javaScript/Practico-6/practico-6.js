5e3491d9fa71ed863f0a5c6959f79ad5dfccc954
Ezequiel Massa <ezequielmassa.dev@gmail.com>
2023-12-06 08:07:31 -0300

    feat/ resuelta consigna 2 practico 6

--- eze-massa-javaScript/Practico-6/practico-6.js
+++ eze-massa-javaScript/Practico-6/practico-6.js
@@ -12,5 +12,18 @@ class Producto {
 const producto1 = new Producto('celular', 100, 3)
 const total = producto1.calcularTotal()
 console.log(`El total es: ${total}`)
+
 //Crear una clase Rectangulo que tenga las propiedades ancho y alto. La clase debe tener un método calcularArea que calcule y devuelva el área del rectángulo.
+class Rectangulo {
+	constructor(ancho, alto) {
+		this.ancho = ancho
+		this.alto = alto
+	}
+	calcularArea = () => {
+		return `El area es : ${this.ancho * this.alto}`
+	}
+}
+const rectangulo1 = new Rectangulo(200, 10)
+console.log(rectangulo1.calcularArea())
+
 // Crear una clase Persona que tenga las propiedades nombre, edad y profesión. La clase debe tener un método presentarse que imprima en la consola un mensaje con los datos de la persona.
