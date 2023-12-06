ddb070a775014e3c6b9acbc3c71892abde24cf23
Ezequiel Massa <ezequielmassa.dev@gmail.com>
2023-12-06 08:00:00 -0300

    feat/ creado practico 6 y resuelta primer consigna

---
+++ eze-massa-javaScript/Practico-6/practico-6.js
@@ -0,0 +1,16 @@
+//Crear una clase Producto que tenga las propiedades nombre, precio y cantidad. La clase debe tener un método calcularTotal que calcule y devuelva el total de la compra de ese producto.
+class Producto {
+	constructor(nombre, precio, cantidad) {
+		this.nombre = nombre
+		this.precio = precio
+		this.cantidad = cantidad
+	}
+	calcularTotal = () => {
+		return this.precio * this.cantidad
+	}
+}
+const producto1 = new Producto('celular', 100, 3)
+const total = producto1.calcularTotal()
+console.log(`El total es: ${total}`)
+//Crear una clase Rectangulo que tenga las propiedades ancho y alto. La clase debe tener un método calcularArea que calcule y devuelva el área del rectángulo.
+// Crear una clase Persona que tenga las propiedades nombre, edad y profesión. La clase debe tener un método presentarse que imprima en la consola un mensaje con los datos de la persona.
